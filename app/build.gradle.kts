5fffadcb4f6d9f92af88c3a89cf1ca2d0d31e723
Jandyra Pestana <jandyraborgespestananeta@gmail.com>
2026-08-10 19:32:36 -0300

    Prática 5 - Parte 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.weatherapp"
-    compileSdk {
-        version = release(37) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.weatherapp"
